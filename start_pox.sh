6a75577dfddca7858b1f7ece49b5db2801ede383
Jason Croft <croftbc@gmail.com>
2016-02-23 15:58:25 -0600

    Cleanup start_pox script

--- start_pox.sh
+++ start_pox.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+POXPATH=~/src/pox
+
 export PYTHONPATH=.:$CWD
-#~/src/pox/pox.py log.level --DEBUG openflow.of_01 --port=6633 pox_rpc
-~/src/pox/pox.py log.level --DEBUG openflow.of_01 --port=6633 poxapp
+${POXPATH}/pox.py log.level --DEBUG openflow.of_01 --port=6633 poxapp
 
