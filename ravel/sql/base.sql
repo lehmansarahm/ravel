3547eeeffd295a6d623600eb6363cbfa427ace91
Jason Croft <croftbc@gmail.com>
2016-03-13 11:32:12 -0500

    Cleanup, comment sql base

--- ravel/sql/base.sql
+++ ravel/sql/base.sql
@@ -1,29 +1,14 @@
-DROP TABLE IF EXISTS clock CASCADE;
-CREATE UNLOGGED TABLE clock (
-       counts   integer,
-       PRIMARY key (counts)
-);
-INSERT into clock (counts) values (0) ; -- initialize clock
+------------------------------------------------------------
+-- BASE TABLES
+------------------------------------------------------------
 
-
-DROP TABLE IF EXISTS p_spv CASCADE;
-CREATE UNLOGGED TABLE p_spv (
-       counts   integer,
-       status   text,
-       PRIMARY key (counts)
-);
-
-CREATE OR REPLACE FUNCTION protocol_fun() RETURNS TRIGGER AS
-$$
-plpy.notice ("engage ravel protocol")
-
-ct = plpy.execute("""select max (counts) from clock""")[0]['max']
-plpy.execute ("INSERT INTO p_spv VALUES (" + str (ct+1) + ", 'on');")
-return None;
-$$
-LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
-
-
+/* Topology table - pairs of connected nodes
+ * sid: switch id
+ * nid: next-hop id
+ * ishost: if nid is a host
+ * isactive: if the sid-nid link is online
+ * bw: link bandwidth
+ */
 DROP TABLE IF EXISTS tp CASCADE;
 CREATE UNLOGGED TABLE tp (
        sid      integer,
@@ -35,37 +20,13 @@ CREATE UNLOGGED TABLE tp (
 );
 CREATE INDEX ON tp(sid, nid);
 
-CREATE TRIGGER tp_up_trigger
-     AFTER UPDATE ON tp
-     FOR EACH ROW
-   EXECUTE PROCEDURE protocol_fun();
 
-
-DROP TABLE IF EXISTS switches CASCADE;
-CREATE UNLOGGED TABLE switches (
-       sid      integer PRIMARY KEY,
-       dpid     varchar(16),
-       ip       varchar(16),
-       mac      varchar(17),
-       name     varchar(16)
-);
-CREATE INDEX ON switches(sid);
-
-DROP TABLE IF EXISTS hosts CASCADE;
-CREATE UNLOGGED TABLE hosts (
-       hid      integer PRIMARY KEY,
-       ip       varchar(16),
-       mac      varchar(17),
-       name     varchar(16)
-);
-CREATE INDEX ON hosts (hid);
-
-DROP VIEW IF EXISTS nodes CASCADE;
-CREATE OR REPLACE VIEW nodes AS (
-       SELECT sid AS id, name FROM SWITCHES UNION
-       SELECT hid AS id, name FROM HOSTS
-);
-
+/* Configuration table - per-switch flow configuration
+ * fid: flow id
+ * pid: id of previous-hop node
+ * sid: switch id
+ * nid: id of next-hop node
+ */
 DROP TABLE IF EXISTS cf CASCADE;
 CREATE UNLOGGED TABLE cf (
        fid      integer,
@@ -75,6 +36,15 @@ CREATE UNLOGGED TABLE cf (
 );
 CREATE INDEX ON cf(fid,sid);
 
+
+/* Reachability matrix - end-to-end reachability matrix
+ * fid: flow id
+ * src: the IP address of the source node
+ * dst: the IP address of the destination node
+ * vol: volume allocated for the flow
+ * FW: if flow should pass through a firewall
+ * LB: if flow should be load balanced
+ */
 DROP TABLE IF EXISTS rm CASCADE;
 CREATE UNLOGGED TABLE rm (
        fid      integer,
@@ -86,6 +56,153 @@ CREATE UNLOGGED TABLE rm (
        PRIMARY KEY (fid)
 );
 CREATE INDEX ON rm (fid,src,dst);
+
+
+
+------------------------------------------------------------
+-- NODE TABLES
+------------------------------------------------------------
+
+/* Switch table
+ * sid: switch id (primary key, NOT datapath id)
+ * dpid: datapath id
+ * ip: switch's IP address
+ * mac: switch's MAC address
+ * name: switch's name (in Mininet) 
+ */
+DROP TABLE IF EXISTS switches CASCADE;
+CREATE UNLOGGED TABLE switches (
+       sid	integer PRIMARY KEY,
+       dpid	varchar(16),
+       ip	varchar(16),
+       mac	varchar(17),
+       name	varchar(16)
+);
+CREATE INDEX ON switches(sid);
+
+
+/* Host table
+ * hid: host id
+ * ip: host's IP address
+ * mac: host's MAC address
+ * name: hostname (in Mininet)
+ */
+CREATE UNLOGGED TABLE hosts (
+       hid	integer PRIMARY KEY,
+       ip	varchar(16),
+       mac	varchar(17),
+       name	varchar(16)
+);
+CREATE INDEX ON hosts (hid);
+
+
+/* Nodes view - all nodes and switches in the network
+ * id: the node's id from its respective table (hosts.hid or switches.sid)
+ * name: the node's name
+ */
+DROP VIEW IF EXISTS nodes CASCADE;
+CREATE OR REPLACE VIEW nodes AS (
+       SELECT sid AS id, name FROM SWITCHES UNION
+       SELECT hid AS id, name FROM HOSTS
+);
+
+
+/* Ports table
+ * sid - switch id
+ * nid - id of next-hop node
+ * port - outport on sid for nid
+ */
+DROP TABLE IF EXISTS ports CASCADE;
+CREATE UNLOGGED TABLE ports (
+       sid      integer,
+       nid      integer,
+       port     integer
+);
+
+
+
+------------------------------------------------------------
+-- ORCHESTRATION PROTOCOL
+------------------------------------------------------------
+
+/* Orchestration token clock */
+DROP TABLE IF EXISTS clock CASCADE;
+CREATE UNLOGGED TABLE clock (
+       counts   integer,
+       PRIMARY key (counts)
+);
+
+
+/* Initialize the clock to 0 */
+INSERT into clock (counts) values (0) ;
+
+
+/* Routing shortest path vector priority table */
+DROP TABLE IF EXISTS p_spv CASCADE;
+CREATE UNLOGGED TABLE p_spv (
+       counts   integer,
+       status   text,
+       PRIMARY key (counts)
+);
+
+
+/* Orchestration enabling function */
+CREATE OR REPLACE FUNCTION protocol_fun() RETURNS TRIGGER AS
+$$
+ct = plpy.execute("""select max (counts) from clock""")[0]['max']
+plpy.execute ("INSERT INTO p_spv VALUES (" + str (ct+1) + ", 'on');")
+return None;
+$$
+LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
+
+
+
+------------------------------------------------------------
+-- USER-FACING REACHABILITY MATRIX
+------------------------------------------------------------
+
+/* User reachability matrix */
+DROP TABLE IF EXISTS urm CASCADE;
+CREATE UNLOGGED TABLE urm (
+       fid      integer,
+       host1    integer,
+       host2    integer,
+       PRIMARY KEY (fid)
+);
+CREATE INDEX ON urm(fid,host1);
+
+
+/* User reachability matrix insertion */
+CREATE OR REPLACE RULE urm_in_rule AS
+       ON INSERT TO urm
+       DO ALSO
+       INSERT INTO rm VALUES (NEW.fid,
+                              NEW.host1,
+                              NEW.host2,
+                              1);
+
+
+/* User reachability matrix deletion */
+CREATE OR REPLACE RULE urm_del_rule AS
+       ON DELETE TO urm
+       DO ALSO DELETE FROM rm WHERE rm.fid = OLD.fid;
+
+/* User reachability matrix update */
+CREATE OR REPLACE RULE urm_up_rule AS
+       ON UPDATE TO urm
+       DO ALSO (
+          DELETE FROM rm WHERE rm.fid = OLD.fid;
+          INSERT INTO rm VALUES (OLD.fid,
+                                 NEW.host1,
+                                 NEW.host2,
+                                 1);
+       );
+
+
+
+------------------------------------------------------------
+-- REACHABILITY MATRIX UPDATES
+------------------------------------------------------------
 
 DROP TABLE IF EXISTS rm_delta CASCADE;
 CREATE UNLOGGED TABLE rm_delta (
@@ -110,39 +227,10 @@ CREATE OR REPLACE RULE rm_del AS
            );
 
 
----------- reachability matrix facing user
 
-DROP TABLE IF EXISTS urm CASCADE;
-CREATE UNLOGGED TABLE urm (
-       fid      integer,
-       host1    integer,
-       host2    integer,
-       PRIMARY KEY (fid)
-);
-CREATE INDEX ON urm(fid,host1);
-
-CREATE OR REPLACE RULE urm_in_rule AS
-       ON INSERT TO urm
-       DO ALSO
-       INSERT INTO rm VALUES (NEW.fid,
-                              NEW.host1,
-                              NEW.host2,
-                              1);
-
-CREATE OR REPLACE RULE urm_del_rule AS
-       ON DELETE TO urm
-       DO ALSO DELETE FROM rm WHERE rm.fid = OLD.fid;
-
-CREATE OR REPLACE RULE urm_up_rule AS
-       ON UPDATE TO urm
-       DO ALSO (
-          DELETE FROM rm WHERE rm.fid = OLD.fid;
-          INSERT INTO rm VALUES (OLD.fid,
-                                 NEW.host1,
-                                 NEW.host2,
-                                 1);
-       );
-
+------------------------------------------------------------
+-- SHORTEST PATH VECTOR
+------------------------------------------------------------
 
 DROP TABLE IF EXISTS spv_tb_ins CASCADE;
 CREATE UNLOGGED TABLE spv_tb_ins (
@@ -152,6 +240,7 @@ CREATE UNLOGGED TABLE spv_tb_ins (
        nid      integer
 );
 
+
 DROP TABLE IF EXISTS spv_tb_del CASCADE;
 CREATE UNLOGGED TABLE spv_tb_del (
        fid      integer,
@@ -159,6 +248,7 @@ CREATE UNLOGGED TABLE spv_tb_del (
        sid      integer,
        nid      integer
 );
+
 
 CREATE OR REPLACE FUNCTION spv_constraint1_fun ()
 RETURNS TRIGGER
@@ -187,20 +277,96 @@ if TD["new"]["status"] == 'on':
 return None;
 $$ LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
 
+
 CREATE TRIGGER spv_constraint1
-     AFTER INSERT ON p_spv
-     FOR EACH ROW
-   EXECUTE PROCEDURE spv_constraint1_fun();
+       AFTER INSERT ON p_spv
+       FOR EACH ROW
+       EXECUTE PROCEDURE spv_constraint1_fun();
+
+
+CREATE OR REPLACE RULE spv_constaint2 AS
+       ON INSERT TO p_spv
+       WHERE NEW.status = 'on'
+       DO ALSO
+           (UPDATE p_spv SET status = 'off' WHERE counts = NEW.counts;
+           DELETE FROM cf WHERE (fid,pid,sid,nid) IN (SELECT * FROM spv_tb_del);
+           INSERT INTO cf (fid,pid,sid,nid) (SELECT * FROM spv_tb_ins);
+           DELETE FROM spv_tb_del ;
+           DELETE FROM spv_tb_ins ;
+           );
+
+
+CREATE OR REPLACE RULE tick_spv AS
+       ON UPDATE TO p_spv
+       WHERE (NEW.status = 'off')
+       DO ALSO
+           INSERT INTO clock values (NEW.counts);
+
+
+DROP VIEW IF EXISTS spv CASCADE;
+CREATE OR REPLACE VIEW spv AS (
+       SELECT fid,
+              src,
+              dst,
+              (SELECT array(SELECT id1 FROM pgr_dijkstra('SELECT 1 as id,
+                                                     sid as source,
+                                                     nid as target,
+                                                     1.0::float8 as cost
+                                                     FROM tp
+                                                     WHERE isactive = 1', src, dst,FALSE, FALSE))) as pv
+       FROM rm
+);
+
+
+DROP VIEW IF EXISTS spv_edge CASCADE;
+CREATE OR REPLACE VIEW spv_edge AS (
+       WITH num_list AS (
+       SELECT UNNEST (ARRAY[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]) AS num
+       )
+       SELECT DISTINCT fid, num, ARRAY[pv[num], pv[num+1], pv[num+2]] as edge
+       FROM spv, num_list
+       WHERE pv != '{}' AND num < array_length (pv, 1) - 1
+       ORDER BY fid, num
+);
+
+
+DROP VIEW IF EXISTS spv_switch CASCADE;
+CREATE OR REPLACE VIEW spv_switch AS (
+       SELECT DISTINCT fid,
+              edge[1] as pid,
+              edge[2] as sid,
+              edge[3] as nid
+       FROM spv_edge
+       ORDER BY fid
+);
+
+
+DROP VIEW IF EXISTS spv_ins CASCADE;
+CREATE OR REPLACE VIEW spv_ins AS (
+       SELECT * FROM spv_switch
+       EXCEPT (SELECT * FROM cf)
+       ORDER BY fid
+);
+
+
+DROP VIEW IF EXISTS spv_del CASCADE;
+CREATE OR REPLACE VIEW spv_del AS (
+       SELECT * FROM cf
+       EXCEPT (SELECT * FROM spv_switch)
+       ORDER BY fid
+);
+
+
+
+------------------------------------------------------------
+-- TOPOLOGY UPDATES/REROUTING
+------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION tp2spv_fun () RETURNS TRIGGER
 AS $$
-
 isactive = TD["new"]["isactive"]
 sid = TD["new"]["sid"]
 nid = TD["new"]["nid"]
-
-plpy.notice ("tp2spv_fun executed")
-
 if isactive == 0:
    fid_delta = plpy.execute ("SELECT fid FROM cf where (sid =" + str (sid) + "and nid =" + str (nid) +") or (sid = "+str (nid)+" and nid = "+str (sid)+");")
    if len (fid_delta) != 0:
@@ -219,82 +385,13 @@ if isactive == 0:
 return None;
 $$ LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
 
+CREATE TRIGGER tp_up_trigger
+       AFTER UPDATE ON tp
+       FOR EACH ROW
+       EXECUTE PROCEDURE protocol_fun();
+
 CREATE TRIGGER tp_up_spv_trigger
-     AFTER UPDATE ON tp
-     FOR EACH ROW
-   EXECUTE PROCEDURE tp2spv_fun();
-
-CREATE OR REPLACE RULE spv_constaint2 AS
-       ON INSERT TO p_spv
-       WHERE NEW.status = 'on'
-       DO ALSO
-           (UPDATE p_spv SET status = 'off' WHERE counts = NEW.counts;
-           DELETE FROM cf WHERE (fid,pid,sid,nid) IN (SELECT * FROM spv_tb_del);
-           INSERT INTO cf (fid,pid,sid,nid) (SELECT * FROM spv_tb_ins);
-           DELETE FROM spv_tb_del ;
-           DELETE FROM spv_tb_ins ;
-           );
-
-CREATE OR REPLACE RULE tick_spv AS
-       ON UPDATE TO p_spv
-       WHERE (NEW.status = 'off')
-       DO ALSO
-           INSERT INTO clock values (NEW.counts);
-
-DROP VIEW IF EXISTS spv CASCADE;
-CREATE OR REPLACE VIEW spv AS (
-       SELECT fid,
-              src,
-              dst,
-              (SELECT array(SELECT id1 FROM pgr_dijkstra('SELECT 1 as id,
-                                                     sid as source,
-                                                     nid as target,
-                                                     1.0::float8 as cost
-                                                     FROM tp
-                                                     WHERE isactive = 1', src, dst,FALSE, FALSE))) as pv
-       FROM rm
-);
-
-DROP VIEW IF EXISTS spv_edge CASCADE;
-CREATE OR REPLACE VIEW spv_edge AS (
-       WITH num_list AS (
-       SELECT UNNEST (ARRAY[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]) AS num
-       )
-       SELECT DISTINCT fid, num, ARRAY[pv[num], pv[num+1], pv[num+2]] as edge
-       FROM spv, num_list
-       WHERE pv != '{}' AND num < array_length (pv, 1) - 1
-       ORDER BY fid, num
-);
-
-DROP VIEW IF EXISTS spv_switch CASCADE;
-CREATE OR REPLACE VIEW spv_switch AS (
-       SELECT DISTINCT fid,
-              edge[1] as pid,
-              edge[2] as sid,
-              edge[3] as nid
-       FROM spv_edge
-       ORDER BY fid
-);
-
-DROP VIEW IF EXISTS spv_ins CASCADE;
-CREATE OR REPLACE VIEW spv_ins AS (
-       SELECT * FROM spv_switch
-       EXCEPT (SELECT * FROM cf)
-       ORDER BY fid
-);
-
-DROP VIEW IF EXISTS spv_del CASCADE;
-CREATE OR REPLACE VIEW spv_del AS (
-       SELECT * FROM cf
-       EXCEPT (SELECT * FROM spv_switch)
-       ORDER BY fid
-);
-
-
-DROP TABLE IF EXISTS ports CASCADE;
-CREATE UNLOGGED TABLE ports (
-       sid      integer,
-       nid      integer,
-       port     integer
-);
+       AFTER UPDATE ON tp
+       FOR EACH ROW
+       EXECUTE PROCEDURE tp2spv_fun();
 
