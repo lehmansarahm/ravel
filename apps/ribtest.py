13fcc197aceda4f2c14ba36d5297c7e9e1ac04d9
Jason Croft <croftbc@gmail.com>
2016-09-17 02:06:49 -0500

    Add precompute tests, fix bugs in orchestration

--- apps/ribtest.py
+++ apps/ribtest.py
@@ -12,25 +12,28 @@ from ravel.log import logger
 from ravel.util import resource_file
 from ravel.messaging import clear_queue
 
+#RIB_SIZE = 1000000
+RIB_SIZE = 1000
+
 def precompute_paths(db):
     db.cursor.execute("""
-DROP TABLE IF EXISTS paths CASCADE;
-CREATE UNLOGGED TABLE paths (
+DROP TABLE IF EXISTS pre_paths CASCADE;
+CREATE UNLOGGED TABLE pre_paths (
         src  integer,
         dst  integer,
         id   integer PRIMARY KEY,
         CONSTRAINT uniq_pairs UNIQUE(src,dst)
 );
-CREATE INDEX ON paths(src, dst);
+CREATE INDEX ON pre_paths(src, dst);
 
-DROP TABLE IF EXISTS pedges CASCADE;
-CREATE UNLOGGED TABLE pedges (
-        id  integer REFERENCES paths(id),
+DROP TABLE IF EXISTS pre_cf CASCADE;
+CREATE UNLOGGED TABLE pre_cf (
+        id  integer REFERENCES pre_paths(id),
         pid integer,
         sid integer,
         nid integer
 );
-CREATE INDEX ON pedges(id);
+CREATE INDEX ON pre_cf(id);
 """)
 
     db.cursor.execute("SELECT hid FROM hosts;")
@@ -45,34 +48,58 @@ CREATE INDEX ON pedges(id);
     g.add_edges_from(edges)
 
     count = 0
+    pre_paths = []
+    pre_cf = []
+
     for src in nodes:
         for dst in nodes:
             if src == dst:
                 continue
 
-            count += 1
-            path = networkx.shortest_path(g, src, dst)
+            if count > 1000000:
+                break
+
+            try:
+                path = networkx.shortest_path(g, src, dst)
+                count += 1
+            except networkx.exception.NetworkXNoPath:
+                continue
+
             path_edges = []
 
             # TODO: how to handle pathlen=2?
             for i in range(2, len(path)):
                 path_edges.append((path[i-2], path[i-1], path[i]))
 
-            db.cursor.execute("INSERT INTO paths(id, src, dst) VALUES "
-                              "({0}, {1}, {2});"
-                              .format(count, src, dst))
-            
+            pre_paths.append((count, src, dst))
             for row in path_edges:
-                db.cursor.execute("INSERT INTO pedges (id, pid, sid, nid) "
-                                  "VALUES ({0}, {1}, {2}, {3});"
-                                  .format(count, row[0], row[1], row[2]))
+                pre_cf.append((count, row[0], row[1], row[2]))
+
+    print "Paths computed, inserting"
+    open("pre_paths.sql", 'w').close()
+    with open("pre_paths.sql", 'w') as f:
+        f.write("INSERT INTO pre_paths(id, src, dst) VALUES {0};".format(
+            ",".join("({0}, {1}, {2})".format(*t) for t in pre_paths)))
+
+    open("pre_cf.sql", 'w').close()
+    with open("pre_cf.sql", 'w') as f:
+        f.write("INSERT INTO pre_cf(id, pid, sid, nid) VALUES {0};".format(
+            ",".join("({0}, {1}, {2}, {3})".format(*t) for t in pre_cf)))
+
+    pre_paths = []
+    pre_cf = []
+    db.cursor.execute(open("pre_paths.sql").read())
+    db.cursor.execute(open("pre_cf.sql").read())
+    os.remove("pre_paths.sql")
+    os.remove("pre_cf.sql")
 
 def cleanup_precomputed_paths(db):
-    db.cursor.execute("DROP TABLE IF EXISTS paths CASCADE;")
-    db.cursor.execute("DROP TABLE IF EXISTS pedges CASCADE;")
+    db.cursor.execute("DROP TABLE IF EXISTS pre_paths CASCADE;")
+    db.cursor.execute("DROP TABLE IF EXISTS pre_cf CASCADE;")
 
 def disable_orchestration(db):
     db.cursor.execute("""
+DROP TABLE IF EXISTS p_routing CASCADE;
 DROP TRIGGER IF EXISTS spv_constraint1 on p_spv;
 DROP FUNCTION IF EXISTS spv_constraint2_fun() CASCADE;
 CREATE OR REPLACE FUNCTION spv_constraint2_fun(f integer, s integer, d integer)
@@ -156,6 +183,9 @@ class KernelTest(object):
             times.append(micro * 1000 / pathlen)
 
         self.db.cursor.execute("SELECT COUNT(*) FROM cf;")
+        fetch = self.db.cursor.fetchall()
+        #print "    ", int(fetch[0][0])
+
         outfile = "{0}_times_{1}.out".format(self.name, trial)
         with open(outfile, 'w') as f:
             f.write("\n".join(str(t) for t in times))
@@ -362,59 +392,67 @@ class OptimizedOrchestrationTest(KernelTest):
     def __init__(self, db, env):
         super(OptimizedOrchestrationTest, self).__init__(db, env)
 
-#CREATE OR REPLACE FUNCTION spv_constraint2_fun(f integer, s integer, d integer, isadd integer)
-# CREATE OR REPLACE RULE rm_ins AS
-#        ON INSERT TO rm
-#        DO ALSO (
-#            SELECT spv_constraint2_fun(NEW.fid, NEW.src, NEW.dst, 1);
-#        );
     def setup(self):
+        self.env.maincli.onecmd("orch auto on")
+        self.env.maincli.onecmd("orch load routing")
+
         self.db.cursor.execute("""
 DROP TRIGGER IF EXISTS spv_constraint1 on p_spv;
 DROP FUNCTION IF EXISTS spv_constraint2_fun() CASCADE;
-DROP RULE IF EXISTS rm_ins ON rm CASCADE;
+DROP TRIGGER IF EXISTS run_routing_trigger on p_routing;
+DROP FUNCTION IF EXISTS cf_ins();
 
 CREATE FUNCTION spv_constraint2_fun()
-RETURNS trigger
+RETURNS TRIGGER
 AS $$
         DECLARE
             pv integer [];
+            row record;
         BEGIN
-            --IF (NEW.isadd = 1) THEN
+            FOR row in (SELECT fid, src, dst, isadd FROM rm_delta)
+            LOOP
+                IF row.isadd = 1 THEN
  	        pv := ARRAY(SELECT id1 FROM
-	                    pgr_dijkstra('SELECT 1 AS id, sid AS source, nid AS target, 1.0::float8 AS cost FROM tp WHERE isactive=1', NEW.src, NEW.dst, FALSE, FALSE));
+	                    pgr_dijkstra('SELECT 1 AS id, sid AS source, nid AS target, 1.0::float8 AS cost FROM tp WHERE isactive=1', row.src, row.dst, FALSE, FALSE));
 
                 IF array_upper(pv, 1) != 0
                 THEN
 	            FOR i IN 3 .. array_upper(pv, 1)
 	            LOOP
-                        INSERT INTO cf (fid, pid, sid, nid) VALUES (NEW.fid, pv[i-2], pv[i-1], pv[i]);
+                        INSERT INTO cf (fid, pid, sid, nid) VALUES (row.fid, pv[i-2], pv[i-1], pv[i]);
                     END LOOP;
                 END IF;
-	    --END IF;
+                END IF;
 
-	    --IF (NEW.isadd = 0) THEN
-	    --   DELETE FROM cf WHERE fid=NEW.fid;
-	    --END IF;
+                IF row.isadd = 0 THEN
+                    DELETE FROM cf WHERE fid=row.fid;
+                END IF;
+            END LOOP;
 
+        DELETE FROM rm_delta;
         RETURN NEW;
         END;
 $$ LANGUAGE plpgsql VOLATILE SECURITY DEFINER;
 
-CREATE TRIGGER spv_constraint2_trig
-    AFTER INSERT ON rm
-    FOR EACH ROW
-    EXECUTE PROCEDURE spv_constraint2_fun();
+CREATE TRIGGER run_routing_trigger
+     AFTER INSERT ON p_routing
+     FOR EACH ROW
+   EXECUTE PROCEDURE spv_constraint2_fun();
 """)
-       
-        self.env.maincli.onecmd("orch auto on")
-        self.env.maincli.onecmd("orch load routing")
 
     def cleanup(self):
         self.env.maincli.onecmd("orch auto off")
-        self.db.cursor.execute("DROP TRIGGER IF EXISTS spv_constraint2_trig on rm")
+        self.db.cursor.execute("DROP TRIGGER IF EXISTS rm_ins ON rm")
+        self.db.cursor.execute("DROP RULE IF EXISTS rm_ins ON rm;")
+        self.db.cursor.execute("DROP TRIGGER IF EXISTS run_routing_trigger ON p_routing;")
+        self.db.cursor.execute("DROP FUNCTION IF EXISTS cf_ins();")
+
+        self.db.cursor.execute("DROP TRIGGER IF EXISTS spv_constraint1 on p_spv;")
+        self.db.cursor.execute("DROP FUNCTION IF EXISTS spv_constraint2_fun() CASCADE;")
+        self.db.cursor.execute("DROP RULE IF EXISTS rm_ins ON rm CASCADE;")
+        self.db.cursor.execute("DROP TRIGGER IF EXISTS run_routing_trigger on p_routing;")
         reenable_orchestration(self.db)
-        
+
 class OrchestrationTest(KernelTest):
     def __init__(self, db, env):
         super(OrchestrationTest, self).__init__(db, env)
@@ -441,8 +479,8 @@ AS $$
         DECLARE
             row record;
         BEGIN
-            FOR row IN (SELECT pid, sid, nid FROM pedges WHERE
-                        id = (SELECT id FROM paths WHERE src=s AND dst=d))
+            FOR row IN (SELECT pid, sid, nid FROM pre_cf WHERE
+                        id = (SELECT id FROM pre_paths WHERE src=s AND dst=d))
             LOOP
                 INSERT INTO cf (fid, pid, sid, nid) VALUES (f, row.pid, row.sid, row.nid);
             END LOOP;
@@ -467,6 +505,67 @@ class OrchestrationPrecomputedTest(KernelTest):
         super(OrchestrationPrecomputedTest, self).__init__(db, env)
 
     def setup(self):
+        self.env.maincli.onecmd("orch auto on")
+        self.env.maincli.onecmd("orch load routing")
+
+        self.db.cursor.execute("""
+DROP TRIGGER IF EXISTS spv_constraint1 on p_spv;
+DROP FUNCTION IF EXISTS spv_constraint2_fun() CASCADE;
+DROP TRIGGER IF EXISTS run_routing_trigger on p_routing;
+DROP FUNCTION IF EXISTS cf_ins();
+
+CREATE OR REPLACE FUNCTION cf_ins()
+RETURNS TRIGGER
+AS $$
+        DECLARE
+            row record;
+            edge record;
+        BEGIN
+            FOR row in (SELECT fid, src, dst, isadd FROM rm_delta)
+            LOOP
+                IF row.isadd = 1 THEN
+                    FOR edge IN (SELECT pid, sid, nid FROM pre_cf WHERE
+                            id = (SELECT id FROM pre_paths WHERE src=row.src AND dst=row.dst))
+                    LOOP
+                        INSERT INTO cf (fid, pid, sid, nid) VALUES (row.fid, edge.pid, edge.sid, edge.nid);
+                    END LOOP;
+                END IF;
+
+                IF row.isadd = 0 THEN
+                    DELETE FROM cf WHERE fid=row.fid;
+                END IF;
+            END LOOP;
+
+            DELETE FROM rm_delta;
+            RETURN NEW;
+        END;
+$$ LANGUAGE plpgsql VOLATILE SECURITY DEFINER;
+
+CREATE TRIGGER run_routing_trigger
+     AFTER INSERT ON p_routing
+     FOR EACH ROW
+   EXECUTE PROCEDURE cf_ins();
+""")
+
+    def cleanup(self):
+        self.env.maincli.onecmd("orch auto off")
+        self.db.cursor.execute("DROP TRIGGER IF EXISTS rm_ins ON rm")
+        self.db.cursor.execute("DROP RULE IF EXISTS rm_ins ON rm;")
+        self.db.cursor.execute("DROP TRIGGER IF EXISTS run_routing_trigger ON p_routing;")
+        self.db.cursor.execute("DROP FUNCTION IF EXISTS cf_ins();")
+        reenable_orchestration(self.db)
+
+class ColumnConstraintPrecomputedTest(KernelTest):
+    def __init__(self, db, env):
+        super(ColumnConstraintPrecomputedTest, self).__init__(db, env)
+
+    def setup(self):
+        self.db.cursor.execute("ALTER TABLE cf ADD CONSTRAINT edge_exist1 FOREIGN KEY(nid, sid) REFERENCES tp(sid, nid);")
+        self.db.cursor.execute("ALTER TABLE cf ADD CONSTRAINT edge_exist2 FOREIGN KEY(sid, pid) REFERENCES tp(sid, nid);")
+        self.db.cursor.execute("ALTER TABLE cf ADD CONSTRAINT cf_pid_uniq UNIQUE(fid, pid);")
+        self.db.cursor.execute("ALTER TABLE cf ADD CONSTRAINT cf_nid_uniq UNIQUE(fid, nid);")
+        self.db.cursor.execute("ALTER TABLE cf ADD CONSTRAINT cf_sid_uniq UNIQUE(fid, sid);")
+
         self.db.cursor.execute("""
 DROP TRIGGER IF EXISTS spv_constraint1 on p_spv;
 DROP FUNCTION IF EXISTS spv_constraint2_fun() CASCADE;
@@ -477,8 +576,8 @@ AS $$
         DECLARE
             row record;
         BEGIN
-            FOR row IN (SELECT pid, sid, nid FROM pedges WHERE
-                        id = (SELECT id FROM paths WHERE src=s AND dst=d))
+            FOR row IN (SELECT pid, sid, nid FROM pre_cf WHERE
+                        id = (SELECT id FROM pre_paths WHERE src=s AND dst=d))
             LOOP
                 INSERT INTO cf (fid, pid, sid, nid) VALUES (f, row.pid, row.sid, row.nid);
             END LOOP;
@@ -493,12 +592,137 @@ CREATE OR REPLACE RULE rm_ins AS
        );
 """)
 
-        self.env.maincli.onecmd("orch auto on")
-        self.env.maincli.onecmd("orch load routing")
+    def cleanup(self):
+        self.db.cursor.execute("ALTER TABLE cf DROP CONSTRAINT edge_exist1;")
+        self.db.cursor.execute("ALTER TABLE cf DROP CONSTRAINT edge_exist2;")
+        self.db.cursor.execute("ALTER TABLE cf DROP CONSTRAINT cf_sid_uniq;")
+        self.db.cursor.execute("ALTER TABLE cf DROP CONSTRAINT cf_nid_uniq;")
+        self.db.cursor.execute("ALTER TABLE cf DROP CONSTRAINT cf_pid_uniq;")
+        self.db.cursor.execute("DROP TRIGGER IF EXISTS rm_ins on rm")
+        reenable_orchestration(self.db)
+
+class PyTriggerPrecomputedTest(KernelTest):
+    def __init__(self, db, env):
+        super(PyTriggerPrecomputedTest, self).__init__(db, env)
+
+    def setup(self):
+        self.db.cursor.execute("""
+DROP TRIGGER IF EXISTS spv_constraint1 on p_spv;
+DROP FUNCTION IF EXISTS spv_constraint2_fun() CASCADE;
+
+CREATE OR REPLACE FUNCTION path_validate(f integer, s integer, d integer)
+RETURNS integer
+AS $$
+failed = False
+error = "no error"
+path = plpy.execute("SELECT * FROM cf WHERE fid=" + str(f) + ";")
+
+path_sn = []
+path_ps = []
+edges = set()
+
+for edge in path:
+    edges.add((edge['pid'], edge['sid']))
+    edges.add((edge['sid'], edge['nid']))
+
+    path_ps.append((edge['pid'], edge['sid']))
+    path_sn.append((edge['sid'], edge['nid']))
+
+# test uniqueness
+sids = []
+
+for edge in path:
+    if edge['sid'] in sids:
+        failed = True
+        error = "sid uniqueness violation"
+
+    sids.append(edge['sid'])
+
+edge_dict = {}
+for e1, e2 in edges:
+    edge_dict[e1] = e2
+
+for e1,e2 in path_sn + path_ps:
+    # e1_exists = plpy.execute("SELECT COUNT(1) FROM tp WHERE nid={0};"
+    #                          .format(e1))
+    # if int(e1_exists[0]['count']) == 0:
+    #     failed = True
+    #     error = "node not in topo {0}".format(e1)
+    # e2_exists = plpy.execute("SELECT COUNT(1) FROM tp WHERE nid={0};"
+    #                          .format(e2))
+    # if int(e2_exists[0]['count']) == 0:
+    #     failed = True
+    #     error = "node not in topo {0}".format(e2)
+
+    edge_exists = plpy.execute("SELECT COUNT(1) FROM tp WHERE nid={0} AND sid={1}"
+                               .format(e1, e2))
+    if int(edge_exists[0]['count']) == 0:
+        failed = True
+        error = "edge not in topo {0} {1}".format(e1, e2)
+
+curr = s
+count = 0
+while True:
+    if curr == d:
+        break
+    elif count > (len(edges)):
+        count += 1
+        failed = True
+        error = "path loop {0}".format(edge_dict)
+    elif curr in edge_dict:
+        count += 1
+        curr = edge_dict[curr]
+    elif curr not in edge_dict and curr != d:
+        count += 1
+        failed = True
+        error = "path continuity violated: {0} s={1},d={2} => {3} {4} {5}".format(
+                curr, s, d, edge_dict, edges, path)
+    else:
+        failed = True
+        error = "this should not be reachable"
+
+if count != len(edges):
+    failed = True
+    error = "wrong path length s:{0}, d:{1}, cnt:{2}, len{3} -- {4}".format(s, d, count, len(edges), edges)
+
+if failed:
+    # rollback
+    plpy.execute("DELETE FROM cf WHERE fid={0}".format(f))
+    plpy.execute("DELETE FROM rm WHERE fid={0}".format(f))
+    plpy.notice("FAILED")
+    raise Exception(error)
+
+return 0
+$$ LANGUAGE plpythonu VOLATILE SECURITY DEFINER;
+
+CREATE OR REPLACE FUNCTION cf_ins(f integer, s integer, d integer)
+RETURNS integer
+AS $$
+        DECLARE
+            row record;
+        BEGIN
+            FOR row IN (SELECT pid, sid, nid FROM pre_cf WHERE
+                        id = (SELECT id FROM pre_paths WHERE src=s AND dst=d))
+            LOOP
+                INSERT INTO cf (fid, pid, sid, nid) VALUES (f, row.pid, row.sid, row.nid);
+            END LOOP;
+
+            PERFORM path_validate(f, s, d);
+            DELETE FROM rm_delta;
+            RETURN f;
+        END;
+$$ LANGUAGE plpgsql VOLATILE SECURITY DEFINER;
+
+CREATE OR REPLACE RULE rm_ins AS
+       ON INSERT TO rm
+       DO ALSO (
+           INSERT INTO rm_delta values (NEW.fid, NEW.src, NEW.dst, NEW.vol, 1);
+           SELECT cf_ins(NEW.fid, NEW.src, NEW.dst);
+       );
+""")
 
     def cleanup(self):
-        self.env.maincli.onecmd("orch auto off")
-        self.db.cursor.execute("DROP TRIGGER IF EXISTS rm_ins on rm")
+        self.db.cursor.execute("DROP FUNCTION IF EXISTS cf_ins() CASCADE;")
         reenable_orchestration(self.db)
 
 class RibTestConsole(AppConsole):
@@ -573,7 +797,7 @@ class RibTestConsole(AppConsole):
 
     def do_test(self, line):
         if line is None or line == "":
-            size = 25
+            size = RIB_SIZE
             print "No size specified, default = ", size
         else:
             try:
@@ -599,14 +823,23 @@ class RibTestConsole(AppConsole):
         self.db.cursor.execute("DROP TRIGGER IF EXISTS add_flow_trigger ON cf;")
         self.db.cursor.execute("DROP TRIGGER IF EXISTS del_flow_trigger ON cf;")
 
-        precompute_paths(self.db)
+        # disable vacuuming
+        vaccuum = "ALTER TABLE {0} SET (autovacuum_enabled = false, " +\
+                 "toast.autovacuum_enabled = false);"
 
+        self.db.cursor.execute(vaccuum.format("cf"))
+        self.db.cursor.execute(vaccuum.format("rm"))
+
+        precompute_paths(self.db)
         tests = [
-            BaselineTest(self.db, self.env),
             BaselinePrecomputedTest(self.db, self.env),
+            OrchestrationPrecomputedTest(self.db, self.env),
+            ColumnConstraintPrecomputedTest(self.db, self.env),
+            PyTriggerPrecomputedTest(self.db, self.env),
+
+            BaselineTest(self.db, self.env),
             OrchestrationTest(self.db, self.env),
             OptimizedOrchestrationTest(self.db, self.env),
-            OrchestrationPrecomputedTest(self.db, self.env),
             ColumnConstraintTest(self.db, self.env),
             PyTriggerTest(self.db, self.env)
         ]
@@ -615,12 +848,22 @@ class RibTestConsole(AppConsole):
         for i in range(trials):
             for t in tests:
                 print i, t.name
-                t.test(paths, i)
+                elapsed = time.time()
+                t.test_cdf(paths, i)
+                elapsed = (time.time() - elapsed) * 1000
+                if elapsed > 60000:
+                    elapsed = "{0} min".format(round(elapsed / 60000.0, 3))
+                elif elapsed > 1000:
+                    elapsed = "{0} sec".format(round(elapsed / 1000.0, 3))
+                else:
+                    elapsed = "{0} ms".format(round(elapsed, 3))
+
+                print "    ", elapsed
                 sys.stdout.flush()
 
         print "---------------------------------------------"
         for test in tests:
-            print test.result()
+            print "*** ", test.result()
         print "---------------------------------------------"
 
         cleanup_precomputed_paths(self.db)
